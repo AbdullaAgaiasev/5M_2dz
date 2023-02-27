369da352ef8029692363c5392900861d6f7436ba
mac <abllagajsaev@gmail.com>
2023-02-27 21:26:48 +0600

    Initial commit

---
+++ app/src/main/java/com/example/a5m_2dz/LoveApi.kt
@@ -0,0 +1,19 @@
+package com.example.a5m_2dz
+
+import retrofit2.Call
+import retrofit2.http.GET
+import retrofit2.http.Header
+import retrofit2.http.Query
+import java.nio.channels.spi.AbstractSelectionKey
+
+interface LoveApi {
+
+    @GET("getPercentage")
+     fun calculatePercentage(
+        @Query("fname")firstName:String,
+        @Query("sname")secondName:String,
+        @Header("X-RapidAPI-Key") key: String = "ed79022b4bmsh600725ba2c9e3dbp12b6a6jsna7a8f4a4a202",
+        @Header("X-RapidAPI-Host") host: String = "love-calculator.p.rapidapi.com"
+    ) : Call<LoveModel>
+
+}
---
+++ app/src/main/java/com/example/a5m_2dz/LoveModel.kt
@@ -0,0 +1,19 @@
+package com.example.a5m_2dz
+
+import com.google.gson.annotations.SerializedName
+
+//{
+//fname:"Aktan"
+//sname:"Vika"
+//percentage:"46"
+//result:"Can choose someone better."
+//}
+
+data class LoveModel(
+    @SerializedName("fname")
+    val firstName:String,
+    @SerializedName("sname")
+    val secondName:String,
+    val percentage:String,
+    val result:String
+) : java.io.Serializable
